2398868efaca34792f5c56cbf8e771de560bbd24
Trashman247 <rnwong@scu.edu>
2023-06-17 12:03:02 -0700

    App.js with functional and object-orientated versions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+// functional below
 function App() {
   return (
     <div className="App">
@@ -10,3 +11,17 @@ function App() {
 }
 
 export default App;
+
+// object orientated below:
+// import React from 'react'
+
+// export default class App extends React.Component {
+//   render() {
+//     return <>
+//       <div className="App">
+//         <h1>Hello World!</h1>
+//         <button>Click to Fetch!</button>
+//       </div>
+//     </>
+//   };
+// };
