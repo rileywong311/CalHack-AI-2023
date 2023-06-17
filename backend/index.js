1b6254cedac1f8a8bae6ff0411514061c22b7a8c
Leo Chung <leochungus@berkeley.edu>
2023-06-17 12:39:08 -0700

    created skeleton

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,34 @@ app.use(express.static('build'));
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/chatgpt', async (request, response) => response.send(`
-    <p>CHATGP SAYS ${await getCompletion("Hello world!")}</p>
-`));
+const recipes = {};
 
-/** REFERENCE FOR USING A BODY
- * app.post('/api/persons', (request, response) => {
+app.get('/api/test', async (request, response) => {
+    return response.json({content: await getCompletion("Hello, world!")})
+});
+
+app.get('/api/steps', async (request, response) => {
     const body = request.body;
-    if (!body.name || !body.number) return response.status(400).json({error: 'content missing'});
-    if (persons.find(person => person.name === body.name)) return response.status(400).json({error: 'names must be unique'});
+    if (!body.food) return response.status(400).json({error: 'content missing'});
+    
+    // 1) Prompt GPT with recipes
+    // 2) Mangle it into a readable JSON for the frontend
+    // 3) Add recipe to the recipes list for future retrieval
+    // 4) PROFIT!
 
-    const person = { id: getRandomArbitrary(100, 500000), name: body.name, number: body.number };
-    persons = persons.concat(persons);
-    response.json(person);
-}); */
+    return response.json({})
+});
+
+app.get('/api/fix', async (request, response) => {
+    const body = request.body;
+    if (!body.id || !body.step || !body.reason) return response.status(400).json({error: 'content missing'});
+
+    // 1) Prompt GPT with the recipe retrieved from ID along with messed up step and reason
+    // 2) Get its new recipe and update the old recipe
+    // 3) PROFIT!
+
+    return response.json({})
+});
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
