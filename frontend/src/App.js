912b5ac2460dd880f5b2621195e88f359ed15286
Trashman247 <rnwong@scu.edu>
2023-06-17 18:24:29 -0700

    Frontend barebones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,52 @@ import './GeneralStyling.css';
 import './App.css';
 import React from 'react'
 import HelpMeDialog from './HelpMeDialog.js'
+import ExpandMenu from './ExpandMenu.js'
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props); //lets you use "this" in the class
     this.state = { 
-      dishName: 'No Dish'
+      dishName: 'No Dish',
+      totalTime: '50min',
+      prepTime: '25min'
     };
   };
 
   render() {
     return <>
-      <div className="App">
+      <div className="main">
+        <div style={{'width': 'min(700px, 80vw)'}}>
+          <span class="big-text">{this.state.dishName}</span>
+        </div>
+        <hr style={{'width': '100%'}} />
+        <div style={{'width': 'min(700px, 80vw)'}}>
+          <ExpandMenu name="Overview:" />
+          <ExpandMenu name="Ingredients:" />
+        </div>
+        <hr style={{'width': '100%'}} />
+      </div>
 
-        <h1 class="big">{this.state.dishName}</h1>
-        
-        <hr style={{'width': '800px'}} />
+      <div className="main">
+        <div style={{'display': 'flex', 'flex-wrap': 'wrap', 'gap': '1rem'}}>
+          <span style={{'background': 'var(--secondary)', 'border-radius': '999px', 'padding': '0.75rem 1rem 0.75rem'}}>
+            Total Time: <strong>{this.state.totalTime}</strong>
+          </span>
+          <span style={{'background': 'var(--secondary)', 'border-radius': '999px', 'padding': '0.75rem 1rem 0.75rem'}}>
+            Prep Time: <strong>{this.state.prepTime}</strong>
+          </span>
+        </div>
+      </div>
+      <div className="main">
+        <div style={{'width': 'min(1000px, 90vw)', 'height': '1000px', 'margin': '25px', 'background': 'white'}}>
 
-        <hr style={{'width': '800px'}} />
-
-        <HelpMeDialog />
+        </div>
+      </div>
+      <div className="main">
+        <div style={{'width': 'min(1000px, 90vw)'}}>
+          <HelpMeDialog />
+        </div>
       </div>
     </>
   };
