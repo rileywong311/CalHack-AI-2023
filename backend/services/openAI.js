135a795a8b4284cf2333cdd0936977fe06571b4c
Leo Chung <leochungus@berkeley.edu>
2023-06-18 10:19:58 -0700

    prototyping fixing endpoint

--- backend/services/openAI.js
+++ backend/services/openAI.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { Configuration, OpenAIApi } from 'openai';
-import { getPrompt } from './prompt.js';
+import { getPrompt, getFixingPrompt } from './prompt.js';
 
 const configuration = new Configuration({apiKey: process.env.API_KEY});
 const openai = new OpenAIApi(configuration);
@@ -12,6 +12,18 @@ export async function getRecipe(food) {
     const chatCompletion = await openai.createChatCompletion({
         model: "gpt-4",
         messages: [{role: "user", content: getPrompt(food)}]
+    });
+    return chatCompletion.data.choices[0].message.content;
+}
+
+/** Fixes a recipe using ChatGPT.
+ *  @param {string} recipe - The recipe to fix.
+ *  @param {number} step - The step to fix the recipe from.
+ *  @returns {Promise<string>} - The output from ChatGPT. */
+export async function getFixedRecipe(recipe, step) {
+    const chatCompletion = await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: [{role: "user", content: getFixingPrompt(recipe, step)}]
     });
     return chatCompletion.data.choices[0].message.content;
 }
